c5870b62ed896b9ba7a62377b72eca87c87f454d
Yadwinder Singh <ryker1990@gmail.com>
2016-11-15 17:07:39 +0530

    Update MainPage.xaml.cs

--- Windows IoT/TMD26721/TMD26721/MainPage.xaml.cs
+++ Windows IoT/TMD26721/TMD26721/MainPage.xaml.cs
@@ -19,10 +19,10 @@ namespace TMD26721
 		private const byte PROX_I2C_ADDR = 0x39;		// I2C address of the TMD26721
 		private const byte PROX_REG_COMMAND = 0x80;		// Command register
 		private const byte PROX_REG_ENABLE = 0x00;		// Enables states and interrupt register
-        private const byte PROX_REG_PTIME = 0x02;		// Proximity ADC time register
+        	private const byte PROX_REG_PTIME = 0x02;		// Proximity ADC time register
 		private const byte PROX_REG_WTIME = 0x03;		// Wait time register
-        private const byte PROX_REG_PPULSE = 0x0E;      // Proximity Pulse register
-        private const byte PROX_REG_CONTROL = 0x0F;		// Control register
+        	private const byte PROX_REG_PPULSE = 0x0E;      	// Proximity Pulse register
+        	private const byte PROX_REG_CONTROL = 0x0F;		// Control register
 		private const byte PROX_REG_PDATA = 0x18;		// Proximity ADC low data register
 
 		private I2cDevice I2CProximity;
@@ -41,7 +41,7 @@ namespace TMD26721
 
 		private async void InitI2CProximity()
 		{
-			string aqs = I2cDevice.GetDeviceSelector();				// Get a selector string that will return all I2C controllers on the system
+			string aqs = I2cDevice.GetDeviceSelector();		// Get a selector string that will return all I2C controllers on the system
 			var dis = await DeviceInformation.FindAllAsync(aqs);	// Find the I2C bus controller device with our selector string
 			if (dis.Count == 0)
 			{
@@ -57,8 +57,8 @@ namespace TMD26721
 				Text_Status.Text = string.Format(
 					"Slave address {0} on I2C Controller {1} is currently in use by " +
 					"another application. Please ensure that no other applications are using I2C.",
-					settings.SlaveAddress,
-					dis[0].Id);
+				settings.SlaveAddress,
+				dis[0].Id);
 				return;
 			}
 
@@ -68,11 +68,11 @@ namespace TMD26721
 				The first byte is the register address we want to write to
 				The second byte is the contents that we want to write to the register
 			*/
-			byte[] WriteBuf_Enable = new byte[] { PROX_REG_ENABLE | PROX_REG_COMMAND, 0x0D };		// 0x0D sets Power ON, Wait and Proximity features are enabled
-			byte[] WriteBuf_Ptime = new byte[] { PROX_REG_PTIME | PROX_REG_COMMAND, 0xFF };			// 0x00 sets PTIME : 2.73 ms, 1 cycle, 1023 max count
-			byte[] WriteBuf_Wtime = new byte[] { PROX_REG_WTIME | PROX_REG_COMMAND, 0xFF };			// 0xFF sets WTIME : 2.73 ms (WLONG = 0), 1 wait time
-            byte[] WriteBuf_Ppulse = new byte[] { PROX_REG_PPULSE | PROX_REG_COMMAND, 0x20 };       // 0x20 sets Proximity pulse count to 32
-            byte[] WriteBuf_Control = new byte[] { PROX_REG_CONTROL | PROX_REG_COMMAND, 0x20 };		// 0x20 sets Proximity uses CH1 diode, Proximity gain 1x
+			byte[] WriteBuf_Enable = new byte[] { PROX_REG_ENABLE | PROX_REG_COMMAND, 0x0D };	// 0x0D sets Power ON, Wait and Proximity features are enabled
+			byte[] WriteBuf_Ptime = new byte[] { PROX_REG_PTIME | PROX_REG_COMMAND, 0xFF };		// 0x00 sets PTIME : 2.73 ms, 1 cycle, 1023 max count
+			byte[] WriteBuf_Wtime = new byte[] { PROX_REG_WTIME | PROX_REG_COMMAND, 0xFF };		// 0xFF sets WTIME : 2.73 ms (WLONG = 0), 1 wait time
+            	byte[] WriteBuf_Ppulse = new byte[] { PROX_REG_PPULSE | PROX_REG_COMMAND, 0x20 };       	// 0x20 sets Proximity pulse count to 32
+            	byte[] WriteBuf_Control = new byte[] { PROX_REG_CONTROL | PROX_REG_COMMAND, 0x20 };		// 0x20 sets Proximity uses CH1 diode, Proximity gain 1x
 
 			// Write the register settings
 			try
@@ -80,8 +80,8 @@ namespace TMD26721
 				I2CProximity.Write(WriteBuf_Enable);
 				I2CProximity.Write(WriteBuf_Ptime);
 				I2CProximity.Write(WriteBuf_Wtime);
-                I2CProximity.Write(WriteBuf_Ppulse);
-                I2CProximity.Write(WriteBuf_Control);
+                		I2CProximity.Write(WriteBuf_Ppulse);
+                		I2CProximity.Write(WriteBuf_Control);
 			}
 			// If the write fails display the error and stop running
 			catch (Exception ex)
@@ -151,4 +151,3 @@ namespace TMD26721
 		}
 	}
 }
-
